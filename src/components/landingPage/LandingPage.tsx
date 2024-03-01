c391766da90dd0b68521da200616fccffebe9ec1
UpekshaHerath <upekshadilshan000@gmail.com>
2024-03-01 23:55:54 +0530

    first commit

---
+++ src/components/landingPage/LandingPage.tsx
@@ -0,0 +1,9 @@
+const LandingPage = () => {
+    return (
+        <div className="bg-gradient-to-br from-blue-300 to-blue-500">
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        </div>
+    );
+};
+
+export default LandingPage;
