93bac67e6c1f6aa5dd52077de6b947e2d82cc86a
UpekshaHerath <upekshadilshan000@gmail.com>
2024-03-09 12:06:37 +0530

    fix: code styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -60,12 +60,11 @@ const content = [
 export default function Home() {
   return (
     <>
-      
-        <div className="h-100">
-          <ThemeSwitcher />
-        </div>
-        {/* landing page */}
-        <div className="h-96 relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
+      <div className="h-100">
+        <ThemeSwitcher />
+      </div>
+      {/* landing page */}
+      <div className="h-96 relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
         <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
         <Boxes />
         <h1 className={cn("md:text-4xl text-xl text-white relative z-20")}>
