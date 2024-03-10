3fe59385540b1efd213215e53cd9fc5a7c9cd54b
UpekshaHerath <upekshadilshan000@gmail.com>
2024-03-10 14:12:03 +0530

    feat: hero section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import { cn } from "@/utils/cn";
-import Landingpage from "../../src/components/landingPage/LandingPage";
 import { Boxes } from "@/components/ui/background-boxes";
 import Image from "next/image";
 import { StickyScroll } from "@/components/ui/sticky-scroll-reveal";
@@ -7,6 +6,103 @@ import { ThemeSwitcher } from "@/components/theme/ThemeSwitcher";
 import { MacbookScroll } from "@/components/ui/macbook-scroll";
 import Link from "next/link";
 import { TracingBeam } from "@/components/ui/tracing-beam";
+import { HeroParallax } from "@/components/ui/hero-parallax";
+
+export const products = [
+  {
+    title: "Moonbeam",
+    link: "https://gomoonbeam.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/moonbeam.png",
+  },
+  {
+    title: "Cursor",
+    link: "https://cursor.so",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/cursor.png",
+  },
+  {
+    title: "Rogue",
+    link: "https://userogue.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/rogue.png",
+  },
+ 
+  {
+    title: "Editorially",
+    link: "https://editorially.org",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/editorially.png",
+  },
+  {
+    title: "Editrix AI",
+    link: "https://editrix.ai",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/editrix.png",
+  },
+  {
+    title: "Pixel Perfect",
+    link: "https://app.pixelperfect.quest",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/pixelperfect.png",
+  },
+ 
+  {
+    title: "Algochurn",
+    link: "https://algochurn.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/algochurn.png",
+  },
+  {
+    title: "Aceternity UI",
+    link: "https://ui.aceternity.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/aceternityui.png",
+  },
+  {
+    title: "Tailwind Master Kit",
+    link: "https://tailwindmasterkit.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
+  },
+  {
+    title: "SmartBridge",
+    link: "https://smartbridgetech.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
+  },
+  {
+    title: "Renderwork Studio",
+    link: "https://renderwork.studio",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/renderwork.png",
+  },
+ 
+  {
+    title: "Creme Digital",
+    link: "https://cremedigital.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/cremedigital.png",
+  },
+  {
+    title: "Golden Bells Academy",
+    link: "https://goldenbellsacademy.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/goldenbellsacademy.png",
+  },
+  {
+    title: "Invoker Labs",
+    link: "https://invoker.lol",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/invoker.png",
+  },
+  {
+    title: "E Free Invoice",
+    link: "https://efreeinvoice.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/efreeinvoice.png",
+  },
+];
 
 const content = [
   {
@@ -63,6 +159,7 @@ export default function Home() {
       <div className="h-100">
         <ThemeSwitcher />
       </div>
+
       {/* landing page */}
       <div className="h-96 relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
         <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
@@ -74,6 +171,7 @@ export default function Home() {
           Information Technology Undergraduate
         </p>
       </div>
+
       <TracingBeam className="px-6">
         <div className="overflow-hidden dark:bg-[#0B0B0F] bg-white w-full">
           <MacbookScroll
@@ -100,12 +198,17 @@ export default function Home() {
             src={`/linear.webp`}
             showGradient={false}
           />
+
+          {/* hero section with projects */}
+          {/* <HeroParallax products={products} /> */}
         </div>
+
         {/* technologies */}
         <div className="p-10">
           <h2 className="text-3xl font-bold">Technologies</h2>
           <StickyScroll content={content} />
         </div>
+
       </TracingBeam>
     </>
   );
